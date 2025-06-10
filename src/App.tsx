a5dffa53d4b09d2e04af22edc74da08d021ae4d1
Riwaj Prasai <riwajfictious@gmail.com>
2025-06-10 20:28:25 +0545

    update tasks descriptions

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,15 @@ function App() {
     {
       title: "JSX, Components & Props",
       link: "/jsx-components-props",
-      desc: "This task involves building a blog page in React using reusable components to display posts and layout elements. Components like Header, PostPreview, Card, and Footer use props to customize their content and appearance. This approach helps keep the code organized and easy to maintain.",
+      desc: "This task focuses on creating a single blog page in React by building reusable components that display post previews and page layout elements. Key components such as Header, PostPreview, Card, and Footer accept props to customize their content and appearance, allowing for flexibility and consistency throughout the page. By using props and children, these components support easy customization while maintaining a clean and organized code structure. This approach not only simplifies maintenance but also makes it easier to extend and update the blog page as needed.",
       image: "assets/screenshots/jsx-components-props.jpg",
     },
-    {
-      title: "Conditional Rendering and Lists",
-      link: "/conditional-rendering-lists",
-      desc: "This task covers the basics conditional rendering and lists in React.",
-      image: "assets/screenshots/conditional-rendering-lists.jpg",
-    },
+    // {
+    //   title: "Conditional Rendering and Lists",
+    //   link: "/conditional-rendering-lists",
+    //   desc: "This task covers the basics conditional rendering and lists in React.",
+    //   image: "assets/screenshots/conditional-rendering-lists.jpg",
+    // },
   ];
 
   return (
