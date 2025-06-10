26b6d6eea7ee81a5a5a93e5864faa4f75c3a79c8
Riwaj Prasai <riwajfictious@gmail.com>
2025-06-10 20:10:41 +0545

    update tasks descriptions

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ function App() {
     {
       title: "JSX, Components & Props",
       link: "/jsx-components-props",
-      desc: "This task covers the basics of JSX, creating components, and passing props in React.",
+      desc: "This task involves building a blog page in React using reusable components to display posts and layout elements. Components like Header, PostPreview, Card, and Footer use props to customize their content and appearance. This approach helps keep the code organized and easy to maintain.",
       image: "assets/screenshots/jsx-components-props.jpg",
     },
     {
       title: "Conditional Rendering and Lists",
       link: "/conditional-rendering-lists",
-      desc: "This task covers the basics of JSX, creating components, and passing props in React.",
+      desc: "This task covers the basics conditional rendering and lists in React.",
       image: "assets/screenshots/conditional-rendering-lists.jpg",
     },
   ];
@@ -25,18 +25,7 @@ function App() {
             <div className="task-content">
               <div className="task-number">Task {index + 1}</div>
               <div className="task-title">{task.title}</div>
-              <div className="task-desc">
-                {task.desc}Lorem Ipsum is simply dummy text of the printing and
-                typesetting industry. Lorem Ipsum has been the industry's
-                standard dummy text ever since the 1500s, when an unknown
-                printer took a galley of type and scrambled it to make a type
-                specimen book. It has survived not only five centuries, but also
-                the leap into electronic typesetting, remaining essentially
-                unchanged. It was popularised in the 1960s with the release of
-                Letraset sheets containing Lorem Ipsum passages, and more
-                recently with desktop publishing software like Aldus PageMaker
-                including versions of Lorem Ipsum.
-              </div>
+              <div className="task-desc">{task.desc}</div>
             </div>
             <div className="task-image">
               <img src={task.image} alt={task.title} />
